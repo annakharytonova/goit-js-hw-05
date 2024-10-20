0cfee6c3333f506a2ceef0b3116f1a9cec744bb5
Anna Kharytonova <haritonova1512@gmail.com>
2024-10-20 21:44:41 +0300

    task-4

--- js/task-3.js
+++ js/task-3.js
@@ -1,5 +1,3 @@
-// Функція має повертати масив усіх користувачів, відсортованих за спаданням кількостій їх друзів (властивість friends).
-
 const sortByDescendingFriendCount = users =>
   users.toSorted((a, b) => b.friends.length - a.friends.length);
 
